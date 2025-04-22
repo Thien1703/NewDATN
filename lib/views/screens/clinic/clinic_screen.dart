cc5cc641665adfed206b7da7e6d6afa458795417
Vipertricks <hodactoan181905@gmail.com>
2025-04-22 14:20:45 +0700

    fix Splash Screen

--- lib/views/screens/clinic/clinic_screen.dart
+++ lib/views/screens/clinic/clinic_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:health_care/common/app_colors.dart';
 import 'package:health_care/models/clinic.dart';
 import 'package:health_care/viewmodels/api/clinic_api.dart';
+import 'package:health_care/views/screens/chat/chat_screen.dart';
 import 'package:health_care/views/widgets/widget_header_body.dart';
 import 'package:health_care/views/screens/appointment/appointment_screen.dart';
 
